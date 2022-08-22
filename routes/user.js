47dcdab68f41cc9f9bbc8c23935e0dfb0ba4b258
KawshithaFdo <praveenkawshika456@gmail.com>
2022-08-22 20:53:17 +0530

    Complete User.js

--- routes/user.js
+++ routes/user.js
@@ -1,35 +1,90 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../models/user.models')
 
 //Get All
-router.get('/',(req,res) => {
-    res.send('Get All User')
+router.get('/',async (req,res) => {
+    try {
+        const users = await User.find()
+        res.json(users)
+    } catch (err) {
+        res.send('Err: ' + err)
+    }
 })
 
 //Add
-router.post('/',(req,res) => {
-    res.send("Post User")
+router.post('/',async (req,res) => {
+    const user = new User({
+        first_name: req.body.first_name,
+        sur_name: req.body.sur_name,
+        Gender: req.body.Gender,
+        date_of_birth: req.body.date_of_birth,
+        password: req.body.password,
+        contact: req.body.contact,
+        email: req.body.email
+    })
+    try {
+        const response = await user.save()
+        res.json(response)
+    } catch (err) {
+        res.send('Err: ' + err)
+    }
 })
 
 //Update
-router.put('/',(req,res) => {
-    res.send("Put User")
+router.put('/:id',async (req,res) => {
+    try {
+        const user = await User.findById(req.params.id)
+            user.first_name= req.body.first_name,
+            user.sur_name= req.body.sur_name,
+            user.Gender= req.body.Gender,
+            user.date_of_birth= req.body.date_of_birth,
+            user.password= req.body.password,
+            user.contact= req.body.contact,
+            user.email= req.body.email
+
+        const response = await user.save()
+        res.json(response)
+
+    } catch (err) {
+        res.send('Err: ' + err)
+    }
+
 })
 
 //Delete
-router.delete('/:id',(req,res) => {
-    res.send("Delete User by ID : "+req.params.id)
+router.delete('/:id',async (req,res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        const response = await user.remove()
+
+        res.json(response)
+    } catch (err) {
+        res.send('Err: ' + err)
+    }
 })
 
 //Get by Id
-router.get('/:id',(req,res) => {
-    res.send("Get User by ID : "+req.params.id)
+router.get('/:id',async (req,res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        res.json(user)
+    } catch (err) {
+        res.send('Err: ' + err)
+    }
 })
 
 //Login
-router.post('/:username/:passwords',(req,res) => {
-    res.send("User Login");
+router.post('/:email/:passwords',async (req,res) => {
+    try {
+         mail=req.params.email
+        const user=await User.find({email:mail})
+        res.json(user)
+    }catch (err) {
+        res.send('Err: ' + err)
+    }
+
 })
 
 
