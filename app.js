684ad8a1b2e9c922b43b372979a2821342e8e193
KawshithaFdo <praveenkawshika456@gmail.com>
2022-08-22 13:04:43 +0530

    Complete mongodb connection

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const user=require('./routes/user')
 const posts=require('./routes/post')
 
 const app = express()
 const port = 5000
-//
-// const url = 'mongodb://localhost/facebookclone'
-//
-// mongoose.connect(url, { useNewUrlParser: true })
-// const con = mongoose.connection
-//
-// con.on("open", () => {
-//     console.log('MongoDB connected!');
-// })
+
+const url = 'mongodb://127.0.0.1/facebookclone'
+
+mongoose.connect(url, { useNewUrlParser: true })
+const con = mongoose.connection
+
+con.on("open", () => {
+    console.log('MongoDB connected!');
+})
 
 app.use(express.json())
 app.use('/user', user)
